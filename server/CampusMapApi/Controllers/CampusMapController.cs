1135b9b96cc93409667f4dda496383de41af7431
JessRuehle <115715411+JessRuehle@users.noreply.github.com>
2025-03-10 18:38:05 -0400

    Debugging

--- server/CampusMapApi/Controllers/CampusMapController.cs
+++ server/CampusMapApi/Controllers/CampusMapController.cs
@@ -17,67 +17,23 @@ public class CampusMapController : ControllerBase
         _logger = logger;
     }
 
-    //
-    // http POST endpoint accessible at POST /api/CampusMap/find-path
-    [HttpPost("find-path")]
-    public IActionResult FindPath(float currLoc, float dest) {
+    
+    // // http POST endpoint accessible at POST /api/CampusMap/find-path
+    // [HttpPost("find-path")]
+    // public Task<IActionResult> FindPath(float currLoc, float dest) {
 
-      var path = new List<LocationNode>();
+    //   var path = new List<LocationNode>();
 
-      return Ok(path);
+    //   return Ok(path);
 
-    }
+    // }
 
-    // queries database for all nodes and returns a list of location objects
-    // http GET endpoint accessible at GET /api/CampusMap/get-locations
-    [HttpGet("get-locations")]
-    public IActionResult GetLocations() {
-
-      // initial db connection
-      var uri = "neo4j+s://apibloomap.xyz:7687";
-      var username = Environment.GetEnvironmentVariable("DB_USER") 
-        ?? throw new InvalidOperationException("DB_USER is not set");
-      var password = Environment.GetEnvironmentVariable("DB_PASSWORD") 
-        ?? throw new InvalidOperationException("DB_PASSWORD is not set");
-        
-      _driver = GraphDatabase.Driver(uri, 
-        AuthTokens.Basic(username, password));
-
-      await using var session = _driver.AsyncSession();
-
-      // query to retrieve all nodes building and room number attributes
-      var query = "MATCH (n) RETURN n.building AS building, n.roomNumber AS roomNumber";
-
-      var locations;
-      
-       try {
-            
-            var result = await session.RunAsync(query);
-            locations = new List<LocationNode>();
-
-            // Process each record in the result set
-            await result.ForEachAsync(record => {
-
-                string building = record["building"].As<string>();
-                string roomNumber = record["roomNumber"].As<string>();
-
-                string formattedRoom = $"{building} Room {roomNumber}";
-
-                LocationNode node = new LocationNode();
-                node.building = building;
-                node.roomNumber = roomNumber;
-                node.displayName = formattedRoom;
-
-                rooms.Add(node);
-            });
-
-        } catch (Exception ex) {
-            Console.WriteLine($"Error: {ex.Message}");
-        }      
-
-
-
-    }
+    // // queries database for all nodes and returns a list of location objects
+    // // http GET endpoint accessible at GET /api/CampusMap/get-locations
+    // [HttpGet("get-locations")]
+    // public Task<IActionResult> GetLocations() {
+     
+    // }
 
 }
 
