96c9502c29f7dd24268bb2e2181f6a29aef360d0
JessRuehle <115715411+JessRuehle@users.noreply.github.com>
2025-03-11 15:30:31 -0400

    Update CampusMapController.cs

--- server/CampusMapApi/Controllers/CampusMapController.cs
+++ server/CampusMapApi/Controllers/CampusMapController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using Neo4j.Driver;
 
 namespace CampusMapApi.Controllers;
@@ -33,9 +34,46 @@ public class CampusMapController : ControllerBase
     // http GET endpoint accessible at GET /api/CampusMap/get-locations
     [HttpGet("get-locations")]
     public Task<IActionResult> GetLocations() {
+        // initial db connection
+      var uri = "neo4j+s://apibloomap.xyz:7687";
+      var username = Environment.GetEnvironmentVariable("DB_USER") 
+        ?? throw new InvalidOperationException("DB_USER is not set");
+      var password = Environment.GetEnvironmentVariable("DB_PASSWORD") 
+        ?? throw new InvalidOperationException("DB_PASSWORD is not set");
+        
+      IDriver _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(username, password));
+      await using var session = _driver.AsyncSession();
+
+      var query = "MATCH (n) RETURN n.building AS building, n.roomNumber AS roomNumber";
+
+      var locations = new List<LocationNode>();
+      
+      try {
+            
+        var result = await session.RunAsync(query);
+
+        // Process each record in the result set
+        await result.ForEachAsync(record => {
+
+          string building = record["building"].As<string>();
+          string roomNumber = record["roomNumber"].As<string>();
+
+          string formattedRoom = $"{building} Room {roomNumber}";
+
+          LocationNode node = new LocationNode();
+          node.building = building;
+          node.roomNumber = roomNumber;
+          node.displayName = formattedRoom;
+          locations.Add(node);
+
+        });
+
+      } catch (Exception ex) {
+          Console.WriteLine($"Error: {ex.Message}");
+      }
         //var path = new List<LocationNode>();
-        var path = new {message = "API Endpoint Response Good!"};
-     return Task.FromResult<IActionResult>(Ok(path));
+        //var path = new {message = "API Endpoint Response Good!"};
+     return Task.FromResult<IActionResult>(Ok(locations));
     }
 
 }
