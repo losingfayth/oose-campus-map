971389a03ee1b6e765b26f160bc081ebf8e7d584
EthanB-03 <ethanb213361@gmail.com>
2025-03-13 11:52:56 -0400

    --------> DISGRAD LAST MESSAGE
    Added a second search bar for room number
    alongside the existing one that is used for
    building. Also reformatted the search bar.
    Added functionality so users can click the search
    list and it fills search bar with clicked item.
    Hides the search list if a user clicked an item
    from the list, redisplays it if the user changes
    their mind and starts backspacing.

--- client/test-map/App.js
+++ client/test-map/App.js
@@ -1,56 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import MapView, {Marker, Polyline} from 'react-native-maps';
-import { StyleSheet, View, Image} from 'react-native';
-import * as Location from 'expo-location';
-import SearchBar from './app/components/SearchBar';
-import BarSearch from './app/components/BarSearch';
+import React, { useEffect, useState } from "react";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import { StyleSheet, View, Image } from "react-native";
+import * as Location from "expo-location";
+import SearchBar from "./app/components/SearchBar";
+import BarSearch from "./app/components/BarSearch";
+import { searchables, roomNumbers } from "./app/components/test/Words";
 
 export default function App() {
-
-  const [location, setLocation] = useState(null)
+  const [location, setLocation] = useState(null);
   const [subscription, setTracker] = useState(null);
-  const [region, setRegion] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 41.007799728334525,
+    longitude: -76.44851515601727,
+    latitudeDelta: 0.00922,
+    longitudeDelta: 0.00421,
+  });
+  const [isRegionSet, setIsRegionSet] = useState(false); // New state to track if the region has been set
 
   const routeCoordinates = [
     // Apple maps
     // { latitude: 41.006960, longitude: -76.448590 }, // Point a
     // { latitude: 41.007050, longitude: -76.448650 }, // Point b
-    // { latitude: 41.007150, longitude: -76.448610 }, 
+    // { latitude: 41.007150, longitude: -76.448610 },
     // { latitude: 41.007230, longitude: -76.448520 }, // Point c
     // { latitude: 41.007249, longitude: -76.448466 }, // Point d
-    { latitude: 41.006900, longitude: -76.449110 }, // first
-    { latitude: 41.006774, longitude: -76.449009 }, 
-    { latitude: 41.006975, longitude: -76.448576 }, 
-    { latitude: 41.007099, longitude: -76.448645 }, 
-    { latitude: 41.007242, longitude: -76.448531 }, 
-    { latitude: 41.007249, longitude: -76.448466 }, 
-    { latitude: 41.007319, longitude: -76.448316 }, 
-    { latitude: 41.007191, longitude: -76.448214 }, 
-    { latitude: 41.007175, longitude: -76.448248 }, 
-    { latitude: 41.007099, longitude: -76.448306 }, 
-    { latitude: 41.006990, longitude: -76.448220 }, 
-    // { latitude: 41.006980, longitude: -76.448245 }, 
-    { latitude: 41.00695003, longitude:  -76.44821454}, 
-    { latitude: 41.00690578, longitude:  -76.44831560}, 
-    { latitude: 41.00693638, longitude:  -76.44833930}, 
-    { latitude: 41.00696368, longitude:  -76.44836176}, 
-    { latitude: 41.00703194, longitude:  -76.44841603}, 
-    { latitude: 41.00700370, longitude:  -76.44849650}, 
+    { latitude: 41.0069, longitude: -76.44911 }, // first
+    { latitude: 41.006774, longitude: -76.449009 },
+    { latitude: 41.006975, longitude: -76.448576 },
+    { latitude: 41.007099, longitude: -76.448645 },
+    { latitude: 41.007242, longitude: -76.448531 },
+    { latitude: 41.007249, longitude: -76.448466 },
+    { latitude: 41.007319, longitude: -76.448316 },
+    { latitude: 41.007191, longitude: -76.448214 },
+    { latitude: 41.007175, longitude: -76.448248 },
+    { latitude: 41.007099, longitude: -76.448306 },
+    { latitude: 41.00699, longitude: -76.44822 },
+    // { latitude: 41.006980, longitude: -76.448245 },
+    { latitude: 41.00695003, longitude: -76.44821454 },
+    { latitude: 41.00690578, longitude: -76.4483156 },
+    { latitude: 41.00693638, longitude: -76.4483393 },
+    { latitude: 41.00696368, longitude: -76.44836176 },
+    { latitude: 41.00703194, longitude: -76.44841603 },
+    { latitude: 41.0070037, longitude: -76.4484965 },
   ];
 
   //const selectedPoints = [routeCoordinates[2], routeCoordinates[3], routeCoordinates[4]];
 
   // set up a useEffect to request permissions, fetch user location, and track location
-  useEffect(() => 
-  {
+  useEffect(() => {
     // request user location to use while app is running
-    async function getPermissionsAndStartWatching()
-    {
+    async function getPermissionsAndStartWatching() {
       // wait until we get permission granted or denied
-      let{status} = await Location.requestForegroundPermissionsAsync()
-      if(status !== 'granted')
-      {
-        console.log('Permission not granted')
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.log("Permission not granted");
         return;
       }
 
@@ -59,15 +62,13 @@ export default function App() {
       setLocation(currentLocation.coords);
 
       // Start watching position updates
-      const newTracker = await Location.watchPositionAsync
-      (
+      const newTracker = await Location.watchPositionAsync(
         {
           accuracy: Location.Accuracy.High,
           timeInterval: 10, // time in milliseconds
           distanceInterval: 1, // update after this many meters moved
         },
-        (location_update) => 
-        {
+        (location_update) => {
           //console.log('Updated location:', location_update.coords);
           setLocation(location_update.coords);
         }
@@ -78,33 +79,31 @@ export default function App() {
     getPermissionsAndStartWatching();
 
     // Stop watching location when the app closes or user navigates to another screen
-    return () => 
-    {
-      if (subscription) 
-        {
-        subscription.remove(); 
+    return () => {
+      if (subscription) {
+        subscription.remove();
       }
     };
-  }, []);   
+  }, []);
+
+  // Handle region change only if the region hasn't been set yet
+  const handleRegionChangeComplete = (newRegion) => {
+    if (!isRegionSet) {
+      setRegion(newRegion);
+      setIsRegionSet(true); // Mark that the region is set
+    }
+  };
 
   return (
     <View style={styles.container}>
       <MapView
-        style = {styles.map}
-        initialRegion={{
-          latitude: 41.007681,
-          longitude: -76.448487,
-          latitudeDelta: 0.00922,
-          longitudeDelta: 0.00421,
-        }}
+        style={styles.map}
+        initialRegion={region} // Set the initial region only once
+        region={isRegionSet ? region : undefined} // After the initial set, use the region prop only
         //showsUserLocation={true}
 
+        onRegionChangeComplete={handleRegionChangeComplete} // update the zoom level when the user changes it
         minZoomLevel={16}
-        onRegionChangeComplete={(newRegion) => setRegion((prev => ({
-          ...prev,
-          latitudeDelta: newRegion.latitudeDelta,
-          longitudeDelta: newRegion.longitudeDelta,
-        })))} // update the zoom level when the user changes it
       >
         {/* Draw the path */}
         <Polyline
@@ -115,36 +114,44 @@ export default function App() {
           strokeColor="blue"
         />
         {/* Marker showing the User's location */}
-          {location && (
-            <Marker
-              coordinate={{
-                latitude: location.latitude,
-                longitude: location.longitude,
-              }}>
-              <Image source={require('./assets/cropped-huskie.png')} style={{height: 40, width:40 }}/>
-            </Marker>
-          )}
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          >
+            <Image
+              source={require("./assets/cropped-huskie.png")}
+              style={{ height: 40, width: 40 }}
+            />
+          </Marker>
+        )}
       </MapView>
 
-      {/* Add search bar */}
-      {/* <Screen> */}
-          <SearchBar />
-          {/* <BarSearch /> */}
-      {/* </Screen> */}
-
+      {/* Add search bar with first being for building and second for room number */}
+      <SearchBar
+        searchFilterData={searchables}
+        searchFilterStyles={{ left: "14.5%", width: "84%" }}
+      />
+      <SearchBar
+        customStyles={{ width: "30%", left: "64%", borderColor: "black" }}
+        showIcon={false}
+        searchFilterData={roomNumbers}
+        searchFilterStyles={{ width: "100%" }}
+        placeholderText="Room"
+      />
     </View>
   );
-
 }
 
-const styles = StyleSheet.create ({
-  container:{
+const styles = StyleSheet.create({
+  container: {
     ...StyleSheet.absoluteFillObject,
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
-  map:{
+  map: {
     ...StyleSheet.absoluteFillObject,
-  }
-})
-
+  },
+});
