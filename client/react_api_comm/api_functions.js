64b50f1bc9974a72fcb62cc6a69fe4a2bfd517f8
JessRuehle <115715411+JessRuehle@users.noreply.github.com>
2025-03-27 20:21:14 -0400

    Added functionality for find-path

--- client/react_api_comm/api_functions.js
+++ client/react_api_comm/api_functions.js
@@ -9,13 +9,13 @@ async function getLocations() {
     console.log("Running getLocations()");
     try {
 
-         // use fetch to make an http request to server at get-location endpoint
+        // use fetch to make an http request to server at get-location endpoint
         const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/get-locations");
         
         
-    console.log("Raw response:", response);  // Check the full response object
-    const data = await response.json();
-    console.log("Parsed JSON:", data);
+        console.log("Raw response:", response);  // Check the full response object
+        const data = await response.json();
+        console.log("Parsed JSON:", data);
         // const data = await response.json();
         // console.log(data.stringify);
 
@@ -33,17 +33,32 @@ Returns
 */
 async function findPath(currLoc, destination) {
 
-    console.log("Running findPath()");
 
-    // use fetch to make an http request to server at find-path endpoint
-    const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/find-path", {
-        method: "POST", // http POST request
-        headers: {"Content-Type": "application/json"}, // sending data as json
-        body: JSON.stringify({currLoc, destination}) // convert js to json before sending
-    });
+    console.log("Running FindPath()");
+    try {
 
-    const data = await response.json();        
-    console.log(data);
+        // use fetch to make an http request to server at find-path endpoint
+        const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/find-path", {
+            method: "POST", // http POST request
+            headers: {"Content-Type": "application/json"}, // sending data as json
+            body: JSON.stringify({currLoc, destination}) // convert js to json before sending
+        });
+        
+        
+        console.log("Raw response:", response);  // Check the full response object
+        const data = await response.json();
+        console.log("Parsed JSON:", data);
+        // const data = await response.json();
+        // console.log(data.stringify);
+
+
+        // throw error if fetch is unsuccessful
+    } catch (error) {
+        console.error("Error fetching path: ", error);
+    }
+
+    // const data = await response.json();        
+    // console.log(data);
 
 
     // // once a response is revieved from the server, convert json object into
