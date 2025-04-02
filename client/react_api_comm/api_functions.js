80f7199fae60977e3164048a9a7284420874439b
JessRuehle <115715411+JessRuehle@users.noreply.github.com>
2025-04-01 22:38:24 -0400

    updated to separate getLocations

--- client/react_api_comm/api_functions.js
+++ client/react_api_comm/api_functions.js
@@ -5,12 +5,12 @@ Makes an Http GET request to server endpoint get-locations.
 Returns a list of nodes representing all locations stored in database. Each node
 contains the following attributes: location code, floor, building, display name, isValidDestination
 */
-async function getLocations() {
-    console.log("Running getLocations()");
+async function getBuildings() {
+    console.log("Running getBuildings()");
     try {
 
         // use fetch to make an http request to server at get-location endpoint
-        const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/get-locations");
+        const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/get-buildings");
         
         
         console.log("Raw response:", response);  // Check the full response object
@@ -22,7 +22,39 @@ async function getLocations() {
 
         // throw error if fetch is unsuccessful
     } catch (error) {
-        console.error("Error fetching locations: ", error);
+        console.error("Error fetching buildings: ", error);
+    }
+
+}
+
+/* 
+Makes an Http GET request to server endpoint get-locations.
+Returns a list of nodes representing all locations stored in database. Each node
+contains the following attributes: location code, floor, building, display name, isValidDestination
+*/
+async function getRooms(building) {
+    console.log("Running getRooms()");
+    try {
+
+        // use fetch to make an http request to server at get-location endpoint
+         // use fetch to make an http request to server at find-path endpoint
+         const response = await fetch("https://apibloomap.xyz:5164/api/CampusMap/get-rooms", {
+            method: "POST", // http POST request
+            headers: {"Content-Type": "application/json"}, // sending data as json
+            body: JSON.stringify({buildings}) // convert js to json before sending
+        });
+        
+        
+        console.log("Raw response:", response);  // Check the full response object
+        const data = await response.json();
+        console.log("Parsed JSON:", data);
+        // const data = await response.json();
+        // console.log(data.stringify);
+
+
+        // throw error if fetch is unsuccessful
+    } catch (error) {
+        console.error("Error fetching rooms: ", error);
     }
 
 }
