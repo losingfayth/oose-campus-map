a0838cf53789231eaf17052708f07f07f532d4a8
EthanB-03 <ethanb213361@gmail.com>
2025-04-05 16:38:05 -0400

    Added new class to namespace

--- server/CampusMapApi/BuildingInfo.cs
+++ server/CampusMapApi/BuildingInfo.cs
@@ -1,4 +1,7 @@
-public class BuildingDTO {
-    public string id { get; set; } = string.Empty;
-    public string building { get; set; } = string.Empty;
+
+namespace CampusMapApi {
+        public class BuildingDTO {
+        public string id { get; set; } = string.Empty;
+        public string building { get; set; } = string.Empty;
+    }
 }
