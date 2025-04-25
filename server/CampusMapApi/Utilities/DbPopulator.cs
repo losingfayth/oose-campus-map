ccd6b56a68743e1b4159b0226a903cdad356d18e
fayth 3 <142681569+losingfayth@users.noreply.github.com>
2025-04-25 12:07:43 -0400

    db populator working

--- server/CampusMapApi/Utilities/DbPopulator.cs
+++ server/CampusMapApi/Utilities/DbPopulator.cs
@@ -27,12 +27,9 @@ namespace CampusMapApi.Utilities
 
 		public async static Task<bool> PopulatePoi(Neo4jService neo4j)
 		{
-			// File Path
 			string fp = "../../db/jsons/poi.json";
 
 			string json = File.ReadAllText(fp);
-
-			//Console.WriteLine(json);
 
 			var options = new JsonSerializerOptions
 			{
@@ -41,42 +38,24 @@ namespace CampusMapApi.Utilities
 			};
 
 			var pois = JsonSerializer.Deserialize<Dictionary<string, PointOfInterest>>(json, options);
-
-			//List<PointOfInterest> pois = JsonSerializer.Deserialize<List<PointOfInterest>>(json);
-
 			
 			const string query = @"
-        MATCH (cat:PointOfInterestCategory) WHERE cat.name = $category
-        MATCH (bldg:Area) WHERE bldg.name = $building
-        MATCH (loc:Location) WHERE loc.name = 'Room ' + $room AND (loc)-[:IS_IN]->(bldg)
-        CREATE (n:PointOfInterest { 
-            name: $name, 
-            abbreviation: $abbreviation
-        })-[:IN_CATEGORY]->(cat)
-        CREATE (n)-[:LOCATED_AT]->(loc)
-		RETURN n
-		";
-
-			/*
-				MATCH (loc:Location) WHERE loc.name = 'Room $room' AND (loc)-[:IS_IN]->(bldg)
-				MATCH (cat:PointOfInterestCategory) where cat.name = $cat
-				CREATE (poi:PointOfInterest {
-					name: $name,
-					abbreviation: $abbr
+				MATCH (cat:PointOfInterestCategory) WHERE cat.name = $category
+				MATCH (bldg:Area) WHERE bldg.name = $building
+				MATCH (loc:Location) WHERE loc.name = 'Room ' + $room AND (loc)-[:IS_IN]->(bldg)
+				CREATE (n:PointOfInterest { 
+					name: $name, 
+					abbreviation: $abbreviation
 				})-[:IN_CATEGORY]->(cat)
-				CREATE (poi)-[:AT_LOCATION]->(loc)
-				RETURN poi
-			*/
-			
-
+				CREATE (n)-[:LOCATED_AT]->(loc)
+				RETURN n
+			";
 
 			foreach (KeyValuePair<string, PointOfInterest> poi in pois)
 			{ 
-				//Console.WriteLine(poi.Value.Name + " " + poi.Value.Category);
 
 				if (poi.Value.Room != "")
 				{
-					//Console.WriteLine(poi.Value.Name);
 					var results = await neo4j.ExecuteWriteQueryAsync(
 						query,
 						new Dictionary<string, object> {
@@ -89,21 +68,6 @@ namespace CampusMapApi.Utilities
 					);
 				}
 			}
-
-			//var neo4j = Neo4jServiceLocator.GetNeo4jService();
-
-			//var query = "MATCH (n:Area) RETURN n LIMIT 25";
-
-			//var result = await neo4j.ExecuteReadQueryAsync(query);
-
-			//Console.WriteLine(result);
-			
-			/*
-			result.values.ForEach(record => {
-				//Console.WriteLine(record["name"]);
-				Console.WriteLine(record.Properties["name"].As<string>());
-			});
-			*/
 
 			return true;
 		}
